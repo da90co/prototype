68c8be8ae197296ce871a78d713e0e9b7f1482df
da90 <dmitrij1990@hotmail.com>
2020-05-05 13:34:13 +0100

    generated some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SubnavComponent } from './navbar/subnav/subnav.component';
+import { SettingsComponent } from './settings/settings.component';
+import { MoreComponent } from './more/more.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SubnavComponent,
+    SettingsComponent,
+    MoreComponent
   ],
   imports: [
     BrowserModule,
